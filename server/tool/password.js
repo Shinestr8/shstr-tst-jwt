b10366b91bc28909ef06aac44a628ea9014f15f9
Corentin <shinedownizstr8@gmail.com>
2022-03-30 13:46:47 +0200

    removed console logs

--- server/tool/password.js
+++ server/tool/password.js
@@ -2,7 +2,6 @@ let crypto = require('crypto');
 const secret = require('../config.json').hash;
 
 function getSalt(length) {
-    console.log("getSalt");
     var result           = '';
     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
@@ -17,12 +16,10 @@ function encrypt(password) {
      let salt = getSalt(32);
      password = password + salt + secret;
      password = crypto.createHash('sha256').update(password).digest('hex');
-     console.log(salt, " ", password);
      return [salt, password];
 }
 
 function check(password, salt, hash){
-    console.log(secret);
     const salted = password + salt + secret;
     const newHash = crypto.createHash('sha256').update(salted).digest('hex');
     if(hash === newHash){
