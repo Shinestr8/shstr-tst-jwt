7d62e8b334fa70f1218a17750c3218076b75ede3
Corentin <shinedownizstr8@gmail.com>
2022-03-30 15:49:19 +0200

    added axios request on user create and manage disabled button

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Register(){
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
 
     function handleUsernameChange(e){
         setUsername(e.target.value);
@@ -13,13 +17,32 @@ export function Register(){
         setPassword(e.target.value);
     }
 
-    function register(e){
+    async function register(e){
         e.preventDefault();
-        console.log(`placeholder register with username ${username} and password ${password}`)
+        try {
+            const res = await axios.post("/user", {username: username, password:password});
+            if(res.status === 201){
+                navigate('/')   
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 
 
-    
+    function isButttonDisabled(){
+        if(!password || !username){
+            return true;
+        }
+        if(password.length < 8){
+            return true;
+        }
+        return false;
+    }
+
+
 
     return(
         <div className='login'>
@@ -39,7 +62,7 @@ export function Register(){
             </div>
             
             <footer>  
-                <button type="submit" onClick={register}>Register</button>
+                <button disabled={isButttonDisabled() ? true : false} type="submit" onClick={register}>Register</button>
             </footer>
             
             </form>
